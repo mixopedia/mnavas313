faebaa6648aad2f7bb745dba50533b00b468dc04
mnavasp <65777623+mixopedia@users.noreply.github.com>
2022-08-03 17:00:49 +0400

    Update script.js
    added admin and it support removed store keeping and data entry

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Admin Assistant", "Store Keeper", "Blogger", "Freelancer", "Data Entry Clerk","Designer"],
+        strings: ["Administrator", "Team Leader", "Blogger", "Freelancer", "IT Support Technician","Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Admin Assistant", "Store Keeperf", "Blogger", "Freelancer", "Data Entry Clerk","Designer"],
+        strings: ["Administrator", "Team Leader", "Blogger", "Freelancer", "IT Support Technician","Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
