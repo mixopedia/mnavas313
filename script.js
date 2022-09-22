6efcd92c637751d05b02bf50404e842828d6bb4b
mnavasp <65777623+mixopedia@users.noreply.github.com>
2022-09-22 13:58:50 +0400

    22-09-22

--- script.js
+++ script.js
@@ -1,73 +1,356 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+<!DOCTYPE html>
+<!-- Created By MNP-Muhammed Navas Pilathottathil -->
+<html lang="en">
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Muhammed Navas Pilathottathil-Personal Portfolio Website</title>
+    <meta name="description" content="I am a motivated and skilled individual seeking new opportunities in IT support. Through completion of the Google IT Support Professional Certificate, I developed skills in customer service, networking, operating systems (including Windows & Linux), system administration, and security. My training included hands-on practice in these areas. I'm passionate about continuously learning in the IT field and supporting people to solve their technical problems.">
+    <!-- favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <!-- favicon-->
+    <!-- google adsense-->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7109887473775667"
+     crossorigin="anonymous"></script>
+    <!-- google adsense-->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Portfo<span>lio.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Muhammed Navas</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <!-- Social Media Icons-->
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Administrator", "Team Leader", "Web Developer", "Freelancer", "Technical Support","Designer"],
-        typeSpeed: 100,
-        backSpeed: 50,
-        loop: true
-    });
+                <!-- Twitter -->
+                <div class="top-sm">
+                    <a href="https://twitter.com/TechmixM" target="_blank">
+                        <i class="fab fa-twitter"></i></a>
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Administrator", "Team Leader", "Web Developer", "Freelancer", "Technical Support","Designer"],
-        typeSpeed: 100,
-        backSpeed: 50,
-        loop: true
-    });
+                    <!-- Linkedin -->
+                    <a href="https://www.linkedin.com/in/mnavasp/" target="_blank">
+                        <i class="fab fa-linkedin-in"></i></a>
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
+                    <!-- Stack overflow -->
+                    <a href="https://stackoverflow.com/users/19281238/mnavasp" target="_blank">
+                        <i class="fab fa-stack-overflow"></i></a>
+
+                    <!-- Youtube -->
+                    <a href="https://www.youtube.com/channel/UCQ1il34_5uAjafynwVrZNpQ" target="_blank">
+                        <i class="fab fa-youtube"></i></a>
+
+                    <!-- Github -->
+                    <a href="https://github.com/mixopedia" target="_blank">
+                        <i class="fab fa-github"></i></a>
+
+                    <!-- Whatsapp -->
+                    <a href="https://wa.me/971585151023" target="_blank">
+                        <i class="fab fa-whatsapp"></i></a>
+                </div>
+                
+                <!-- Social Media Icons-->
+                <div class="hireme">
+                <a href="mailto:mnavas313@gmail.com?subject=subject&message=message">Hire me</a>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="profile1.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Muhammed Navas and I'm a <span class="typing-2"></span></div>
+                    <p>To get associated with a progressive organization with better growth prospects, prove my
+                        capability, utilizing my educational knowledge, both theoretically and practically sharpens my
+                        skills to excel in my
+                        field of expertise. Seeking a position to utilize my skills and abilities in the industry that
+                        offers a
+                        professional growth while being resourceful, innovative and flexible.</p>
+                    <a href="Muhammed_Navas_Resume.pdf">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Design</div>
+                        <p>Maintaining the appearance of websites by enforcing content standards. Designing visual
+                            imagery for websites and ensuring that they are in line with branding for clients. Working
+                            with different content management systems. Communicating design ideas using user flows,
+                            process flows, site maps, and wireframes.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">IT Support</div>
+                        <p>Performs a variety of moderately complex information technology support duties to ensure
+                            smooth delivery of technology services. Monitors, operates, or coordinates and assists
+                            others in the operation of computer hardware, software, and peripherals in order to achieve
+                            desired results.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Store Keeper</div>
+                        <p>Being a Storekeeper checks materials and supplies and reports when stock is low. Receives and
+                            unpacks materials and supplies. Additionally, Storekeeper reports damages and discrepancies
+                            for accounting, reimbursement and record-keeping purposes. Requires a high school diploma or
+                            equivalent.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Administrator</div>
+                        <p>Administrative Assistants are responsible for many clerical tasks to ensure the staff can
+                            communicate and work efficiently. Their job involves answering phone calls, greeting
+                            visitors, distributing mail, and preparing communications.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p>Ability to think about a task or a problem in a new or different way, or the ability to use the
+                        imagination to generate new ideas. Creativity enables you to solve complex problems or find
+                        interesting ways to approach tasks</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>WordPress</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Touch Typing</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My journey</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="https://media-exp1.licdn.com/dms/image/D4D0BAQGpXR24hJkwSA/company-logo_200_200/0/1663156010050?e=1671667200&v=beta&t=8T4vkGwGzzg5FOCGHQh1MDRhE-GqNrttZX66HrXI5bI" alt="">
+                        <div class="text">Unilabs Middle Eest.</div>
+                        <p>Unilabs is the medical laboratory in Dubai offering leading Laboratory, Imaging and Pathology services across sixteen countries.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="pmc.jpg" alt="">
+                        <div class="text">Perumanna Medicals</div>
+                        <p>Perumanna Medical Centre is a Primary Care hospital , Pharmacy (Retail &
+                            Wholesale), Laboratory , Emergency services</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="KABCO.jpg" alt="">
+                        <div class="text">KABCO Projects</div>
+                        <p>KABCO Projects is a South Indian-based, PWD , CPWD construction services company and is a leading builder in diverse market segments.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <!-- Social Media Icons-->
+
+            <!-- Twitter -->
+            <ul class="sm">
+                <a href="https://twitter.com/TechmixM" target="_blank">
+                    <i class="fab fa-twitter"></i></a>
+
+                <!-- Linkedin -->
+                <a href="https://www.linkedin.com/in/mnavasp/" target="_blank">
+                    <i class="fab fa-linkedin-in"></i></a>
+
+                <!-- Stack overflow -->
+                <a href="https://stackoverflow.com/users/19281238/mnavasp" target="_blank">
+                    <i class="fab fa-stack-overflow"></i></a>
+
+                <!-- Youtube -->
+                <a href="https://www.youtube.com/channel/UCQ1il34_5uAjafynwVrZNpQ" target="_blank">
+                    <i class="fab fa-youtube"></i></a>
+
+                <!-- Github -->
+                <a href="https://github.com/mixopedia" target="_blank">
+                    <i class="fab fa-github"></i></a>
+
+                <!-- Whatsapp -->
+                <a href="https://wa.me/+971585151023" target="_blank">
+                    <i class="fab fa-whatsapp"></i></a>
+            </ul>
+            <!-- Social Media Icons-->
+
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Muhammed Navas</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Abu Dhabi , UAE</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title"> <a href="mailto:mnavas313@gmail.com?subject=subject&message=message">mnavas313@gmail.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="https://mixopedia.000webhostapp.com/files/php/contact.php" method="post">
+                        <div class="fields">
+                            <div class="field name">
+                                <input name="name" type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input name="email" type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input name="subject" type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea name="message" cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.linkedin.com/in/mnavasp/">MixoPedia</a> | <span
+                class="far fa-copyright"></span> 2020 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
